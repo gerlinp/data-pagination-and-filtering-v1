9405370bcc69fe66c6c2df38339c749f6645fde4
gerlinp <gerlin.pierrelouis@gmail.com>
2021-06-05 22:29:48 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+let show;
+
+function showPage(list,page) {
+
+   for (let i = 0; i <=8; i++) {
+      console.log(data[i])
+   }
+};
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+
+
+// Call functions
