0664b51f2c180b7a4d66d98207518e18d2bf2541
gerlinp <gerlin.pierrelouis@gmail.com>
2021-06-07 10:11:50 -0400

    Pagination buttons and event listener

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
 /*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
@@ -25,30 +10,50 @@ function showPage(list,page) {
    let studentList = document.querySelector('.student-list')
    studentList.innerHTML = '';
    for (let i = start; i < end; i++) {
-         if (i >= start && i <= end) {
-            console.log(`test ${i}`);
+         if (i > 41) {
+            break;
+         } else if (i >= start && i <= end) {
             studentList.innerHTML += `
                <li class="student-item cf">
                   <div class="student-details">
                      <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
                      <h3>${list[i].name.first} ${list[i].name.last}</h3>
-                     <span class="email">ethel.dean@example.com</span>
+                     <span class="email">${list[i].email}</span>
                   </div>
                   <div class="joined-details">
-                     <span class="date">Joined 12-15-2005</span>
+                     <span class="date">${list[i].registered.date}</span>
                   </div>
                </li>`
             ;
          } 
       };
    };
- showPage(data,1);
+
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+let linkList = document.querySelector('.link-list');
+function addPagination(list) {
 
+   let pageButtons = 5;
+   linkList.innerHTML = '';
+   for (let i = 1; i <= pageButtons; i++) {
+      linkList.innerHTML += `
+      <li>
+         <button type="button">${i}</button>
+      </li>`
+   };
+};
+
+
+linkList.addEventListener('click', function(e) {
+   showPage(data,e.target.innerHTML);
+   } 
+);
 
 
 // Call functions
+showPage(data,1);
+addPagination(data);
