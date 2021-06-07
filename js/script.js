0ab0d96dbeb88598e556549a4a4377ae37a457b6
gerlinp <gerlin.pierrelouis@gmail.com>
2021-06-06 23:36:02 -0400

    floyds career tarnished

--- js/script.js
+++ js/script.js
@@ -17,14 +17,31 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-let show;
+let studentList = document.querySelector('.student-list')
 
 function showPage(list,page) {
-
-   for (let i = 0; i <=8; i++) {
-      console.log(data[i])
+   let start = (page * 9) - 9;
+   let end = page * 9;
+   let studentList = document.querySelector('.student-list')
+   studentList.innerHTML = '';
+   for (let i = 0; i >= start; i++) {
+      if (i >= start || i < end) {
+         studentList.innerHTML += `
+         <li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+           <h3>${list[i].picture.large}</h3>
+           <span class="email">ethel.dean@example.com</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined 12-15-2005</span>
+         </div>
+       </li>
+       `;
+      }
    }
-};
+
+   };
 
 
 /*
